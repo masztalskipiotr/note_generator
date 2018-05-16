4c0450a3f6ac0e972b0b04a3adb645ac256bf1aa
Piotr Masztalski <masztalski.piotr@gmail.com>
2018-05-17 00:23:30 +0200

    random rythm

--- project.py
+++ project.py
@@ -3,7 +3,6 @@ from random import randint, random
 class Note:
     time_stamp = 4
     pitch = 'c'
-    pass
 
 
 header = '\\version "2.18.2" \
@@ -17,19 +16,60 @@ header = '\\version "2.18.2" \
   \context { \
     \Score \
   } \
+}\\new Voice \with { \
+  \\remove "Note_heads_engraver" \
+  \\consists "Completion_heads_engraver" \
+  \\remove "Rest_engraver" \
+  \\consists "Completion_rest_engraver" \
 }'
 
-tempo = 4
+all_notes = [' a,,', ' b,,', ' c,,', ' d,,', ' e,,', ' f,,', ' g,,',
+             ' a,', ' b,', ' c,', ' d,', ' e,', ' f,', ' g,',
+             ' a', ' b', ' c', ' d', ' e', ' f', ' g',
+             " a'", " b'", " c'", " d'", " e'", " f'", " g'",
+             " a''", " b''", " c''", " d''", " e''", " f''", " g''",
+             " a'''", " b'''", " c'''", " d'''", " e'''", " f'''", " g'''"]
+
+notes = all_notes[21:28]  # oktawa razkreślna dla testów
+tempo = 5  # 3/4
+
+if tempo == 2:
+    rythmic_values = [[2, '2'], [8/3, '4.'], [4, '4'],
+                      [16/3, '8.'], [8, '8'], [16, '16']]
+elif tempo == 3:
+    rythmic_values = [[4/3, '2.'], [2, '2'], [8/3, '4.'], [4, '4'],
+                      [16/3, '8.'], [8, '8'], [16, '16']]
+elif tempo == 4:
+    rythmic_values = [[1, '1'], [4/3, '2.'], [2, '2'], [8/3, '4.'], [4, '4'],
+                      [16/3, '8.'], [8, '8'], [16, '16']]
+elif tempo == 5:
+    rythmic_values = [[1, '1'], [4 / 3, '2.'], [2, '2'], [8 / 3, '4.'], [4, '4'],
+                      [16 / 3, '8.'], [8, '8'], [16, '16']]
+
 bar_count = 12
 # first_note =
 # ambitus =
 
+# TODO: naprawić kod, tak żeby nuty kończyły się dokładnie na ostatnim takcie (ostatnia nuta nie może być dłuższa niż pozostały czas w takcie)
+
 with open('p1.ly', 'w') as file1:
     file1.write(header)
-    file1.write("\\relative c' { \\time %d/4" % tempo)
-    for i in range(0, bar_count):
-        while bar_count > 0:
-            bar_count -= 1/16
-            file1.write(' c16 ')
-    file1.write('}')
+    file1.write("{ \\time %d/4" % tempo)
+
+    full_time = bar_count/(4/tempo)
+
+    while full_time > 0:
+        note = Note()
+        note.pitch = notes[randint(0, len(notes) - 1)]
+        note.time_stamp, time_str = rythmic_values[randint(0, len(rythmic_values) - 1)]
+        print(full_time, 1/note.time_stamp)
+
+        if 1/note.time_stamp > full_time:
+            note.pitch = ' R'
+            time_str = ''
+
+        full_time -= 1/note.time_stamp
+        file1.write(note.pitch + time_str)
+
+    file1.write('\\bar "||" }')
 
