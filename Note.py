71737aff0da3c0f047021c1b632e0e0eed2e9ad9
Hubert Siuzdak <hubert.siuzdak@gmail.com>
2018-05-21 16:48:28 +0200

    enabled setting pitch and duration of the first note

--- Note.py
+++ Note.py
@@ -1,7 +1,30 @@
+import config
+from config import first_note, first_note_time_stamp
+
+notes = [["c", 0], ["d", 2], ["e", 4], ["f", 5], ["g", 7], ["a",9], ["b",11]]
+
 class Note:
     def __init__(self):
-        self.time_stamp = 4
-        self.pitch = "c'"
-        self.degree = 7
-        self.distance = 12
+        self.time_stamp = config.first_note_time_stamp
+        self.pitch = config.first_note
+        self.degree = count_degree(config.first_note)
+        self.distance = count_distance(self)
         self.octave = ""
+
+def count_degree(first_note):
+    for idx, el in enumerate(notes):
+        if first_note[:1] == notes[idx][0]:
+            degree = idx
+            break
+    for char in first_note[1:]:
+        if char == "'": degree += 7
+        if char == ",": degree -= 7
+    return degree
+
+
+def count_distance(note):
+    distance = notes[note.degree % 7][1]
+    for char in note.pitch[1:]:
+        if char == "'": distance += 12
+        if char == ",": distance -= 12
+    return distance
